60703336e962d4638060bd4b8ebc9bbb15a16386
Em Snook <me@michaelsnook.com>
2025-01-14 13:38:03 +0530

    Fix responsive font color

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const labelVariants = cva(
-	'text-sm font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mb-1 pe-1'
+	'text-sm font-semibold text-foreground leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mb-1 pe-1'
 )
 
 const Label = React.forwardRef<
