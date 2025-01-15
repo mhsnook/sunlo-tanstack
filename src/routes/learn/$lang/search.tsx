5c1a1b79a82c78e5b788016b70e4e5a28949907f
Em Snook <me@michaelsnook.com>
2025-01-15 14:26:58 +0530

    Make the search page work using on-device cache

--- src/routes/learn/$lang/search.tsx
+++ src/routes/learn/$lang/search.tsx
@@ -1,9 +1,4 @@
 import { createFileRoute, Link, useRouter } from '@tanstack/react-router'
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { Controller, useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import toast from 'react-hot-toast'
 
 import { NotebookPen, Plus, Search } from 'lucide-react'
 import {
@@ -17,7 +12,9 @@ import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import languages from '@/lib/languages'
-import { PhraseFull, UserCardInsert } from '@/types/main'
+import { uuid } from '@/types/main'
+import { useLanguage, useLanguagePhrasesMap } from '@/lib/use-language'
+import { useMemo } from 'react'
 
 interface SearchParams {
 	text?: string
@@ -32,40 +29,39 @@ export const Route = createFileRoute('/learn/$lang/search')({
 	component: SearchTab,
 })
 
-const SearchPhraseSchema = z.object({
-	text: z.string().min(1, 'Please enter a phrase to search or add'),
-})
-
-type SearchFormInputs = z.infer<typeof SearchPhraseSchema>
+type SearchablePhrase = {
+	pid: uuid
+	text: string
+}
 
 function SearchTab() {
 	const { navigate } = useRouter()
 	const { lang } = Route.useParams()
-	const { text } = Route.useSearch()
+	const { text: filter } = Route.useSearch()
 
-	const { control, handleSubmit } = useForm<SearchFormInputs>({
-		resolver: zodResolver(SearchPhraseSchema),
-		defaultValues: { text },
-	})
+	const {
+		data: { phrasesMap, pids },
+	} = useLanguage(lang)
+	const searchablePhrases: Array<SearchablePhrase> = useMemo(() => {
+		return pids.map((pid: uuid) => {
+			return {
+				pid,
+				text: [
+					phrasesMap[pid].text,
+					...phrasesMap[pid].translations.map((t) => t.text),
+				].join(', '),
+			}
+		})
+	}, [phrasesMap])
 
-	const searchResults = useQuery({
-		queryKey: ['user', lang, 'search', text],
-		queryFn: (): PhraseFull[] => {
-			return []
-		},
-		gcTime: 120_000,
-		refetchOnWindowFocus: false,
-	})
-
-	const addCardMutation = useMutation({
-		mutationFn: async (data: UserCardInsert) => {
-			return new Promise((resolve) => setTimeout(() => resolve(data), 200))
-		},
-		onSuccess: () => toast.success('Your phrase has been added to your deck.'),
-		onError: (error) => toast.error(error.message),
-	})
-	const onSearchSubmit = (values: SearchFormInputs) =>
-		console.log(`Submitting search...`, values)
+	const searchResults = useMemo(() => {
+		if (!filter.trim()) return pids
+		return searchablePhrases
+			.filter((searchable: SearchablePhrase) => {
+				return searchable.text.toUpperCase().indexOf(filter.toUpperCase()) > -1
+			})
+			.map((s) => s.pid)
+	}, [filter, searchablePhrases, pids])
 
 	return (
 		<Card>
@@ -73,72 +69,52 @@ function SearchTab() {
 				<CardTitle>Search {languages[lang]}</CardTitle>
 				<CardDescription>Search for a phrases to learn</CardDescription>
 			</CardHeader>
-			<CardContent>
-				<form
-					noValidate
-					// eslint-disable-next-line @typescript-eslint/no-misused-promises
-					onSubmit={handleSubmit(onSearchSubmit)}
-					className="space-y-4"
-				>
-					<div>
-						<Label htmlFor="phrase">Phrase</Label>
-						<Controller
-							name="text"
-							control={control}
-							render={({ field }) => (
-								<Input
-									{...field}
-									placeholder="Enter a phrase to search or add"
-									autoFocus
-									onChange={(e) => {
-										field.onChange(e)
-										void navigate({
-											to: '.',
-											replace: true,
-											search: (search: SearchParams) => ({
-												...search,
-												text: e.target.value,
-											}),
-										})
-									}}
-								/>
-							)}
-						/>
-					</div>
-					<div className="flex flex-row gap-2">
-						<Button type="submit">
-							<Search /> Search Phrase
-						</Button>
-						<Button variant="link" asChild>
-							<Link
-								to="/learn/$lang/add-phrase"
-								from={Route.fullPath}
-								search={(search: SearchParams) => ({ ...search, text })}
-							>
-								<NotebookPen />
-								Add New Phrase
-							</Link>
-						</Button>
-					</div>
-				</form>
+			<CardContent className="space-y-6">
+				<div>
+					<Label htmlFor="phrase">Phrase</Label>
+					<Input
+						placeholder="Enter a phrase to search or add"
+						autoFocus
+						onChange={(e) => {
+							void navigate({
+								to: '.',
+								replace: true,
+								search: (search: SearchParams) => ({
+									...search,
+									text: e.target.value,
+								}),
+							})
+						}}
+					/>
+				</div>
+				<div className="flex flex-row gap-2">
+					<Button type="submit">
+						<Search /> Search Phrase
+					</Button>
+					<Button variant="link" asChild>
+						<Link
+							to="/learn/$lang/add-phrase"
+							from={Route.fullPath}
+							search={(search: SearchParams) => ({ ...search, filter })}
+						>
+							<NotebookPen />
+							Add New Phrase
+						</Link>
+					</Button>
+				</div>
 
-				{searchResults.data?.length > 0 ?
+				{searchResults?.length > 0 ?
 					<ul className="space-y-2 mt-4">
-						{searchResults.data.map((result, index) => (
+						{searchResults.map((pid) => (
 							<li
-								key={index}
+								key={pid}
 								className="flex justify-between items-center bg-secondary p-2 rounded"
 							>
 								<span>
-									<strong>{result.text}</strong> - {result.translations[0].text}
+									<strong>{phrasesMap[pid].text}</strong> -{' '}
+									{phrasesMap[pid].translations[0].text}
 								</span>
-								<Button
-									size="sm"
-									variant="ghost"
-									onClick={() =>
-										addCardMutation.mutate({ phrase_id: result.id })
-									}
-								>
+								<Button size="sm" variant="ghost">
 									<Plus className="h-4 w-4" />
 									<span className="sr-only">Add to deck</span>
 								</Button>
