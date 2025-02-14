3f780d80087d583886361f04b975ed05b06345b8
Em Snook <me@michaelsnook.com>
2025-02-14 18:28:02 +0530

    WIP: move appnav into logged-in layout

--- src/routes/_user/friends.tsx
+++ src/routes/_user/friends.tsx
@@ -2,8 +2,6 @@ import { NavbarData } from '@/types/main'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 import { relationsQuery } from '@/lib/friends'
 import { HeartHandshake } from 'lucide-react'
-import { useLinks } from '@/hooks/links'
-import { AppNav } from '@/components/app-nav'
 
 export const Route = createFileRoute('/_user/friends')({
 	component: FriendsPage,
@@ -15,16 +13,11 @@ export const Route = createFileRoute('/_user/friends')({
 				title: `Manage Friends and Contacts`,
 				Icon: HeartHandshake,
 			} as NavbarData,
+			appnav: ['/friends', '/friends/invite', '/friends/search'],
 		}
 	},
 })
 
 function FriendsPage() {
-	const navlinks = useLinks(['/friends', '/friends/invite', '/friends/search'])
-	return (
-		<>
-			<AppNav links={navlinks} />
-			<Outlet />
-		</>
-	)
+	return <Outlet />
 }
