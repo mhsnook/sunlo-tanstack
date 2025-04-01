e2d9ec3157ec73c1c4df8a98af5a9da9c56b329a
em snook <michael.snook@gmail.com>
2025-04-01 21:21:38 +0530

    WIP: Add some more config for deep links
    This is a WIP because the assetlinks.json file doesn't actually include
    the Android signing certificate fingerprint (yet).

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,11 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .setup(|app| {
+          #[cfg(desktop)]
+          app.deep_link().register("sunlo")?;
+          Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
